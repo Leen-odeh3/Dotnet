2a4baca57987de8e198feb61b637434a36962aaf
Leen Odeh <leenodeh287@gmail.com>
2025-03-16 05:37:47 +0200

    Connection Pooling

---
+++ DotNet.Host/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using DotNet.Host.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace DotNet.Host.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var products = await _context.Products.ToListAsync();
+
+        stopwatch.Stop();
+        return Ok(new { TimeTaken = stopwatch.ElapsedMilliseconds, Products = products });
+    }
+}
---
+++ DotNet.Host/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using DotNet.Host.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DotNet.Host.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+}

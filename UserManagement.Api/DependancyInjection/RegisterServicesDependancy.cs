d6dee70eaca2e1cc9320ea7c65b90ea97af856a5
Leen Odeh <leenodeh287@gmail.com>
2025-04-01 16:12:11 +0300

    [#1] Seed User Role

---
+++ UserManagement.Api/DependancyInjection/RegisterServicesDependancy.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UserManagement.Infrastructure.Data;
+
+namespace UserManagement.Api.DependancyInjection;
+public static class RegisterServicesDependancy
+{
+    public static IServiceCollection addDependancy(this IServiceCollection service, IConfiguration config)
+    {
+        // For Identity
+        service.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+        service.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+        //Add Config for Required Email
+        service.Configure<IdentityOptions>(
+            opts => opts.SignIn.RequireConfirmedEmail = true
+            );
+        // Adding Authentication
+        service.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = config["JWT:ValidAudience"],
+                ValidIssuer = config["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"]))
+            };
+        });
+
+
+
+        return service;
+    }
+}
---
+++ UserManagement.Core/DTOs/Auth/Login.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Core.DTOs.Auth;
+public class Login
+{
+    [Required(ErrorMessage = "User Name is required")]
+    public string? Username { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
+}
